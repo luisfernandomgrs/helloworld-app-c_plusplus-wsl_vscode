539169a73075374517b040c4f26e0794b837f641
Luis Fernando M <luisfernandomgrs@gmail.com>
2021-05-06 21:05:01 -0300

    exercise to learning about variables by reference

--- helloworld.cpp
+++ helloworld.cpp
@@ -15,13 +15,27 @@ int main()
       "VS Code",
       "and the C++ extension!"};
 
+  // working with value by reference...
+  // similar to using pointer variable, but not can't null.
+  string &sElementText = msg[5];
+
+  sElementText = "changed value...";
+
   for (const string &word : msg)
   {
     cout << word << " ";
     // printf("%s ", word.c_str());
   }
 
-  // printf("\nTerminated.\n\n");
-  cout << "\nTerminated."
-       << "\n\n";
+  sElementText = "and the C++ extension!";
+  cout << "\n"
+       << sElementText << "\n";
+  cout << "\nreturned value -> ";
+
+  for (const string &word : msg)
+  {
+    cout << word << " ";
+  }
+
+  cout << "\n\n:: Terminated.\n\n";
 }
